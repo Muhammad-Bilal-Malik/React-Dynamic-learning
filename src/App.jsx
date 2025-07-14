ba998221a6280d122ce3cd098b3472c4028c577a
Muhammad-Bilal-Malik <bilalyadali786@gmail.com>
2025-07-15 00:24:16 +0500

    created modal and body scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import Form from "./Form";
-import SingleRecordTable from "./SingleRecordTable";
+
 import Table from "./Table";
 
 function App() {
   return (
     <>
       <Table />
-      <SingleRecordTable />
       <Form />
     </>
   );
